c507ba9fa4f25fca6902e9e8b907eb368d582cb1
Lucer <104328289+Lucer007@users.noreply.github.com>
2024-11-25 20:08:27 -0500

    Changes to Private Key
    Never upload a private key :)

--- Connecting_to_firestore.py
+++ Connecting_to_firestore.py
@@ -4,7 +4,7 @@ from firebase_admin import (credentials, firestore)
 def firstconnect_to_firestore():
     # Initialize Firestore with your service account key
     cred = credentials.Certificate(
-        r"C:\Users\lc201\Documents\Python Project\Sensor-Firestore\save-a-slot-firebase-adminsdk-xeee6-864bff52bb.json")
+        r"C:\Users\lc201\Documents\Python Project\Sensor-Firestore\save-a-slot-firebase-adminsdk-xeee6-a6a543c43c.json")
     firebase_admin.initialize_app(cred)
 
     # Now you can interact with your Firestore database
